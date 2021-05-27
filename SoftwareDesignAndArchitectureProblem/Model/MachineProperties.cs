8037dcae0250ca48f5fb463b475307d421e70fbb
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-27 17:31:40 +0530

    Refactoring

---
+++ SoftwareDesignAndArchitectureProblem/Model/MachineProperties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetMachineNameAssestNameLastestAssest.Model
+{
+    public class MachineProperties
+    {
+        public string MachineName { get; }
+        public string Assetname { get; }
+        public string Series { get; }
+
+
+        public MachineProperties(string machineName, string assetName, string series)
+        {
+            this.MachineName = machineName;
+            this.Assetname = assetName;
+            this.Series = series;
+
+        }
+    }
+
+}
---
+++ SoftwareDesignAndArchitectureProblem/Service/Calculations.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using GetMachineNameAssestNameLastestAssest.Model;
+namespace GetMachineNameAssestNameLastestAssest.Service
+{
+    public class Calculations
+    {
+        string UserInput;
+        char UserInputOption;
+        List<MachineProperties> machines;
+        public Calculations(char userinputoption, string userinput, List<MachineProperties> machines)
+        {
+            this.UserInputOption = userinputoption;
+            this.UserInput = userinput;
+            this.machines = machines;
+
+        }
+
+        public List<string> getAssetName()
+        {
+            List<string> ans = new List<string>();
+            
+            
+            foreach (MachineProperties machine in machines)
+            {
+                if (machine.MachineName == UserInput)
+                {
+                    ans.Add(machine.Assetname);
+                }
+            }
+
+            
+            
+            return ans;
+        }
+        public List<string> getMachineName()
+        {
+            List<string> ans = new List<string>();
+            
+            
+            foreach (MachineProperties machine in machines)
+            {
+                if (machine.Assetname == UserInput)
+                {
+                    ans.Add(machine.MachineName);
+                }
+
+            }
+            
+            return ans;
+        }
+
+        public List<MachineProperties> getMachineTypeWithLatestSeries()
+        {
+            List<MachineProperties> result = new List<MachineProperties>();
+            result.Add(machines[0]);
+            for (int i = 1; i < machines.Count; i++)
+            {
+                for (int j = 0; j < result.Count; j++)
+                {
+                    if (String.Compare(result[j].Assetname, machines[i].Assetname) == 0)
+                    {
+                        if (String.Compare(result[j].Series, machines[i].Series) < 0)
+                        {
+                            result.RemoveAt(j);
+                            result.Add(machines[i]);
+                            break;
+                        }
+
+                    }
+                }
+            }
+            return result;
+        }
+
+
+    }
+}
