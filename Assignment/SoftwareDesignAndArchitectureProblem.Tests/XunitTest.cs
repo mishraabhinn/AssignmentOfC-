92d034771a0d59b0010435717e8dc09e80c1f259
Abhinn Mishra <aviabhinn98@gmail.com>
2021-05-27 09:20:07 +0530

    ProjectC#

---
+++ Assignment/SoftwareDesignAndArchitectureProblem.Tests/XunitTest.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+using Machines;
+using System.Collections.Generic;
+
+namespace SoftwareDesignAndArchitectureProblem.Tests
+{
+    public class TestingSoftwareDesignAndArchitectureProblem
+    {
+        [Fact]
+        public void CheckingTheSizeOfCsvFile()
+        {
+
+            //arrange
+            string userinput="Cutter head";
+            string filePath = @"/Users/abhinnmishra/Downloads/CsharpHandson/ProjectKlingelnberg/Assignment/Data.csv";
+            CsvReader reader = new CsvReader(filePath, userinput);
+            List<Structure> machines = reader.ReadAllMachines();
+            int actual = 9;
+            //act
+
+            int expected = machines.Count;
+
+            //assert
+
+            Assert.Equal(actual, expected);
+
+        }
+        [Fact]
+        public void CheckingForOptionAAndB()
+        {
+
+            //arrange
+            string userinput = "Cutter head";
+            string filePath = @"/Users/abhinnmishra/Downloads/CsharpHandson/ProjectKlingelnberg/Assignment/Data.csv";
+            CsvReader reader = new CsvReader(filePath, userinput);
+            List<Structure> machines = reader.ReadAllMachines();
+            var obj = new Calculations('a',"C300",machines);
+            int actual = 3;
+            //act
+
+
+            HashSet<string> expected = obj.ForOptionAAndB();
+
+            //assert
+
+            Assert.Equal(actual, expected.Count);
+
+        }
+
+        [Fact]
+        public void CheckingForOptionC()
+        {
+
+            //arrange
+            string userinput = "Cutter head";
+            string filePath = @"/Users/abhinnmishra/Downloads/CsharpHandson/ProjectKlingelnberg/Assignment/Data.csv";
+            CsvReader reader = new CsvReader(filePath, userinput);
+            List<Structure> machines = reader.ReadAllMachines();
+            var obj = new Calculations('c', "C300", machines);
+            string actual = "C60";
+            //act
+
+
+            List<Structure> expected = obj.ForOptionC();
+
+            //assert
+
+            Assert.Equal(actual, expected[0].MachineName);
+
+        }
+
+
+    }
+}
---
+++ Assignment/SoftwareDesignAndArchitectureProblem/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machines
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Which Operation You Want To Perform");
+			Console.WriteLine("Press a to Get the list of Asset names for given machine type.");
+			Console.WriteLine("Press b to Get the list of all machine types on which given asset is used.");
+			Console.WriteLine("Press C To Get the machine types which are using the latest series of all the assets that it uses.");
+
+
+
+			char userinputoption = Console.ReadLine()[0];
+			string userinput="empty";
+
+			if (userinputoption == 'a')
+			{
+				Console.WriteLine("Enter The Machine Code:");
+				userinput = Console.ReadLine();
+			}
+
+
+
+			else if (userinputoption == 'b')
+			{
+				Console.WriteLine("Enter The AssetName:");
+				userinput = Console.ReadLine();
+			}
+
+			else
+			{
+				Console.WriteLine("Machine types which are using the latest series of all the assets:");
+			}
+			
+
+
+			string filePath = @"/Users/abhinnmishra/Downloads/CsharpHandson/ProjectKlingelnberg/Assignment/Data.csv";
+			CsvReader reader = new CsvReader(filePath, userinput);
+
+			List<Structure> machines = reader.ReadAllMachines();
+			var obj = new Calculations(userinputoption,userinput,machines);
+
+			
+			if (userinputoption =='a')
+            {
+				HashSet<string> ans =obj.ForOptionAAndB();
+				foreach (var itr in ans)
+				{
+					Console.WriteLine(itr);
+				}
+
+			}
+			else if (userinputoption == 'b')
+			{
+				HashSet<string> ans = obj.ForOptionAAndB();
+				foreach (var itr in ans)
+				{
+					Console.WriteLine(itr);
+				}
+
+			}
+			else
+            {
+				List<Structure> result = obj.ForOptionC();
+				foreach(var itr in result)
+                {
+					Console.WriteLine(itr.MachineName);
+                }
+
+			}
+		}
+	}
+}
